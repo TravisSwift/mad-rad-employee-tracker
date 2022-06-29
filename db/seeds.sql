3412a9b910e7e66d951833c3a38f1cc6acd30cb7
Travis Swift <travis.arthur.swift@gmail.com>
2022-06-29 18:02:36 -0500

    created tables

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,17 +1,33 @@
-INSERT INTO department (name, description)
+INSERT INTO department (name,)
+VALUES 
+('Sales'),
+('Customer_Service'),
+('HR'),
+('Research'),
+('IT'),
+('Payroll');
 
-
-INSERT INTO employees (first_name, last_name, role)
+INSERT INTO role (title, salary)
 VALUES
-  ('Ronald', 'Firbank', employee),
-  ('Virginia', 'Woolf', employee),
-  ('Piers', 'Gaveston', employee),
-  ('Charles', 'LeRoi', employee),
-  ('Katherine', 'Mansfield', employee),
-  ('Dora', 'Carrington', manager),
-  ('Edward', 'Bellamy', manager),
-  ('Montague', 'Summers', manager),
-  ('Octavia', 'Butler', manager),
-  ('Unica', 'Zurn', manager);
+('Manager', '90000'),
+('Supervisor', '60000'),
+('Customer_Service', '45000'),
+('IT_Developer', '75000'),
+('HR_Rep', "50000"),
+('Sales', '70000');
+
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES
+  ('Ronald', 'Firbank', 1),
+  ('Virginia', 'Woolf', 2),
+  ('Piers', 'Gaveston', 3),
+  ('Charles', 'LeRoi', 4),
+  ('Katherine', 'Mansfield', 5),
+  ('Dora', 'Carrington', 6),
+  ('Edward', 'Bellamy', 6),
+  ('Montague', 'Summers', 5),
+  ('Octavia', 'Butler', 3),
+  ('Unica', 'Zurn', 1);
 
  
---
+++ index.js
@@ -0,0 +1,102 @@
+import fetch from 'node-fetch';
+// let fetch = import('node-fetch')
+
+// const mysql = require('mysql2');
+const inquirer = require('inquirer');
+
+import inquirer from 'inquirer';
+ 
+
+
+// Create Connection 
+const db = mysql.createConnection ({
+    host :"localhost",
+    user  :"root",
+    password :"password",
+    database :'department'
+
+});
+
+const start = () => {
+    inquirer.prompt(
+        {
+            name: 'mainquestion',
+            type: 'list',
+            message: 'User sees this question',
+            choices: ['this is a choice', 'second choice', 'third']
+        }
+
+    ).then((answer) => {
+        console.log(answer)
+        connection.end();
+    })
+}
+ 
+
+
+
+// const app = express();
+
+class Department {
+    constructor(id, name) {
+        this.name=name
+        this.value=id
+    }
+}
+
+class Role {
+    constructor(id, role, title, salary) {
+        this.value=id
+        this.role=role
+        this.title=title
+        this.salary=salary
+    }
+}
+
+class Employee {
+    constructor(first_name, last_name, role_id, manager_id) {
+    this.value=id
+    this.first_name=first_name
+    this.last_name=last_name
+    this.role_id=role_id
+    this.manager_id=manager_id
+    }
+}
+
+
+// Create DB
+
+const newDepartment = () => {
+    connection.query('SELECT * FROM department', (err, res) => {
+        
+    } );
+
+}
+
+connection.connect ((err) => {
+    if(err) throw err
+    start()
+})
+
+// connect
+
+// db.connect(() => {
+//     if(err) {
+//         throw err;
+//     }
+//         console.log('MySQL Connected...')
+//     });
+
+// app.get('/createdb') (req, res) => {
+    
+//     let sql = 'CREATE DATABASE department';
+//     db.query(sql, (err, result) => {
+//         if(err) throw err;
+//         console.log(result)
+//         res.send('database created...');
+//     });
+// };
+
+// listen('3000', () => {
+//     console.log('server started on port 3000');
+// });
